d1a7516a84f1809b4bb0b9291a543d17e48060b6
FAHADPN <fahadpuzhakkaraillath@gmail.com>
2023-12-17 00:30:48 +0530

    Added gitignore and removed disabling of stop streaming button

--- app/views.py
+++ app/views.py
@@ -13,17 +13,18 @@ class StreamGeneratorView(APIView):
 
     def name_generator(self,fake):
         name = fake.name()
-        while True:
+        
+        for i in range(100):
             for i in name:
                 yield i
-                # time.sleep(0.1)
+                time.sleep(0.1)
             name = fake.name()
 
     def get(self,request): 
         fake = Faker()
         name = self.name_generator(fake)
         response =  StreamingHttpResponse(name,status=200, content_type='text/event-stream')
-        response['Cache-Control']= 'no-cache',
+        response['Cache-Control']= 'no-cache'
         return response
     
 class HomeView(View):
